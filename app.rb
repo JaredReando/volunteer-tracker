58e03f77ca01d39f20cbcd597a849e8c14fd5b2b
Jared Reando <jaredreando@gmail.com>
2018-12-07 13:03:57 -0800

    Add delete functionality to project edit page. Pass all but 1 integration spec.

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require("./lib/volunteer")
 require("./lib/project")
 require("pg")
 
+
 DB = PG.connect({:dbname => "volunteer_tracker"})
 
 
@@ -14,18 +15,41 @@ get("/") do
 end
 
 post("/") do
-  title = params["title"]
-  project = Project.new({title: title, id: nil})
-  project.save
-  redirect "/"
+title = params["title"]
+project = Project.new({title: title, id: nil})
+project.save
+redirect "/"
 end
 
-get("/project_info/:id") do
+get("/projects/:id") do
+@edit = false
+project_id = params[:id].to_i
+@project = Project.find(project_id)
+(erb :project_info)
+end
+
+get("/projects/:id/edit") do
+  @edit = true
   project_id = params[:id].to_i
   @project = Project.find(project_id)
   (erb :project_info)
-
 end
+
+post("/projects/:id") do
+project_id = params[:id].to_i
+new_title = params[:title]
+@project = Project.find(project_id)
+@project.update({title: new_title})
+(erb :project_info)
+end
+
+delete("/projects/:id/edit") do
+project_id = params[:id].to_i
+project = Project.find(project_id)
+project.delete
+redirect "/"
+end
+
 
 # post("/doctor") do
 #   if (params[:doc_name] == "") || (params[:doc_specialty] == "")
