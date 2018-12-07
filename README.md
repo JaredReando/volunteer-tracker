9de75f9f0afea477515d778da6fddc074ee01d09
Jared Reando <jaredreando@gmail.com>
2018-12-07 08:59:44 -0800

    Add Volunteer class methods. Pass first 6 class specs.

---
+++ README.md
@@ -0,0 +1,52 @@
+# Volunteer Tracker
+
+#### _Database app for tracking projects and volunteers._
+
+#### By _**Jared Reando**_
+
+## Description
+
+_Easily keep track of multiple projects and volunteers_
+
+View this site hosted _[here](https://glacial-ravine-14472.herokuapp.com/)_ on Heroku.
+
+## Setup/Installation Requirements
+
+* Clone this project to your local drive. Link: https://github.com/JaredReando/volunteer-tracker/
+* To make changes of your own to this project, open the downloaded directory files into a developer text-editing program.
+  I use _[Atom](https://atom.io/)_ (It's free!)
+
+## Support and contact details
+
+_Questions or feedback: jaredreando@gmail.com_
+
+## Technologies
+
+- This project was created with Ruby 2.5.3. It is deployed to Heroku using the Sinatra web framework.
+
+## Objectives
+
+- [x] Objects are created from a custom class with custom methods.
+
+- [] Tests are 100% passing. You may not significantly alter the included tests but you may add additional tests as necessary.
+
+- [] Database is set up correctly.
+
+- [] Correct set up of a one-to-many relationship.
+
+- [] Sinatra has required CRUD functionality.
+
+- [] Project has strong commit history history.
+
+- [] Project has detailed README with all necessary setup instructions and a description of the project.
+
+- [] Required functionality was in place by the Friday deadline.
+
+- [] Student can demonstrate understanding of Ruby concepts if asked.
+
+
+### License
+
+Copyright (c) **Jared Reando**
+
+Licensed under the MIT License
--- app.rb
+++ app.rb
@@ -1,61 +1,59 @@
 require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
-require("./lib/doctor")
-require("./lib/patient")
+require("./lib/volunteer")
+require("./lib/project")
 require("pg")
 
-DB = PG.connect({:dbname => "doc_office"})
+DB = PG.connect({:dbname => "volunteer_tracker"})
 
 
 get("/") do
-@doctors = Doctor.all
-@patients = Patient.all
 (erb :index)
 end
 
-post("/doctor") do
-  if (params[:doc_name] == "") || (params[:doc_specialty] == "")
-    redirect "/"
-  else
-    doc_entry = params[:doc_name]
-    doc_specialty = params[:doc_specialty]
-    new_doctor = Doctor.new(:name => doc_entry, :specialty => doc_specialty, :id => nil)
-    new_doctor.save
-    # @doctors = Doctor.all
-    redirect "/"
-  end
-end
-
-post("/patient") do
-  if (params[:patient_name] == "") || (params[:birth_date] == "")
-    redirect "/"
-  else
-    name = params[:patient_name]
-    birth_date = params[:patient_dob]
-    new_patient = Patient.new(:name => name, :birth_date => birth_date, :id => nil)
-    new_patient.save
-    redirect "/"
-  end
-end
-
-get("/tasks/:id") do
-@list_id = params[:id].to_i
-@list = List.find_list(@list_id)
-@tasks = @list.tasks
-
-(erb :tasks)
-end
-
-
-post("/tasks/:id") do
-list_id = params[:id]
-task_description = params[:task_entry]
-  if (task_description == "")
-    redirect "/tasks/#{list_id}"
-  else
-    task = Task.new(:description => task_description, :list_id => list_id)
-    task.save
-    redirect "/tasks/#{list_id}"
-  end
-end
+# post("/doctor") do
+#   if (params[:doc_name] == "") || (params[:doc_specialty] == "")
+#     redirect "/"
+#   else
+#     doc_entry = params[:doc_name]
+#     doc_specialty = params[:doc_specialty]
+#     new_doctor = Doctor.new(:name => doc_entry, :specialty => doc_specialty, :id => nil)
+#     new_doctor.save
+#     # @doctors = Doctor.all
+#     redirect "/"
+#   end
+# end
+#
+# post("/patient") do
+#   if (params[:patient_name] == "") || (params[:birth_date] == "")
+#     redirect "/"
+#   else
+#     name = params[:patient_name]
+#     birth_date = params[:patient_dob]
+#     new_patient = Patient.new(:name => name, :birth_date => birth_date, :id => nil)
+#     new_patient.save
+#     redirect "/"
+#   end
+# end
+#
+# get("/tasks/:id") do
+# @list_id = params[:id].to_i
+# @list = List.find_list(@list_id)
+# @tasks = @list.tasks
+#
+# (erb :tasks)
+# end
+#
+#
+# post("/tasks/:id") do
+# list_id = params[:id]
+# task_description = params[:task_entry]
+#   if (task_description == "")
+#     redirect "/tasks/#{list_id}"
+#   else
+#     task = Task.new(:description => task_description, :list_id => list_id)
+#     task.save
+#     redirect "/tasks/#{list_id}"
+#   end
+# end
